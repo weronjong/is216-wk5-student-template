a7f762c9aaaf1a393db4da2d2153b51f6a15811e
weronjong <jeronapple13@gmail.com>
2026-09-14 09:41:33 +0800

    Complete Week 5 exercises 1-4

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -6,19 +6,16 @@ const id = ref('circle')
 
 
 function mOver() {
-    // add code here
-    
+    id.value = 'square'
 }
 
 function mOut() {
-    // add code here
-   
+    id.value = 'circle'
 }
 </script>
 
 <template>
-    <!-- modify code below -->
-    <div v-bind:id="id">
+    <div v-bind:id="id" v-on:mouseover="mOver" v-on:mouseout="mOut">
         Mouse Over Me
     </div>
 
